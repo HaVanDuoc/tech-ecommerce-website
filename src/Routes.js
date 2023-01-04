7a9fb35e63120f7ba242573a9c1abea8a33c2789
Hà Văn Được <havanduoc.dev@gmail.com>
2023-01-04 13:47:25 +0700

    Update routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,3 +1,13 @@
-import { Home } from "./pages";
+import { Accessory, Home, Laptop, Mobile, PC, Tablet, Watch } from "./pages";
 
-export const publicRoutes = [{ path: "/", page: Home }];
+export const publicRoutes = [
+  { path: "/", page: Home },
+  { path: "/dien-thoai", page: Mobile },
+  { path: "/tablet", page: Tablet },
+  { path: "/laptop", page: Laptop },
+  { path: "/phu-kien", page: Accessory },
+  { path: "/dong-ho", page: Watch },
+  { path: "/pc", page: PC },
+];
+
+export const privateRoutes = [];
