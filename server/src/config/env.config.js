bc09a58c2e1d966c61bb3275f9669db35c1f6016
Hà Văn Được <havanduoc.dev@gmail.com>
2024-03-23 23:46:07 +0700

    fix docker

--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -2,11 +2,11 @@ require("dotenv").config()
 
 module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        username: "root",
+        password: "123456",
+        database: "tech",
+        host: "localhost",
+        port: "3306",
         dialect: "mysql",
         logging: false,
         timezone: "+07:00",
